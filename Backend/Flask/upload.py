c5bd3124e42fc98a4d70b9edc8f978a6df3e9288
pparam-2610 <pparam2610@gmail.com>
2022-08-23 23:06:50 +0530

    predict model API Integrated

--- Backend/Flask/upload.py
+++ Backend/Flask/upload.py
@@ -1,5 +1,11 @@
 from flask import *  
-app = Flask(__name__)  
+from model import model
+import sys
+sys.path.append("..")
+
+app = Flask(__name__)
+depPredict = model.DepPredict()
+# print(depPredict.prediction("1","2","3"))  
 
 @app.route('/')  
 def upload():  
@@ -11,6 +17,22 @@ def success():
         f = request.files['file']  
         f.save(f.filename)  
         return render_template("success.html", name = f.filename)  
+
+@app.route('/evaluate', methods = ['POST'])
+def evaluate():
+    if request.method == 'POST':  
+        nii = request.files['nii']  
+        bval = request.files['bval']  
+        bvec = request.files['bvec'] 
+
+        nii.save("./model/" + nii.filename)  
+        bval.save("./model/" + bval.filename)
+        bvec.save("./model/" + bvec.filename)
+        output = depPredict.prediction(nii.filename, bval.filename, bvec.filename)
+        print(output)    
+        return render_template("success.html", name_nii = nii.filename, name_bval = bval.filename, name_bvec = bvec.filename, output = output[0])  
+        # return render_template("success.html", output = output)  
+
 
 if __name__ == '__main__':  
     app.run(host= '0.0.0.0', debug = True)  
